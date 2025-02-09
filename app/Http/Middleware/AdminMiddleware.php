374fb32ac4f4eaed559ec3733f0f7458fce7d32c
rasyasasikirana <141130044+rasyasasikirana@users.noreply.github.com>
2025-02-09 11:59:10 +0700

    spatie

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -11,18 +11,10 @@ class AdminMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Pastikan pengguna terautentikasi dan memiliki role admin
-        if (auth()->check() && auth()->user()->role === 'admin') {
-            return $next($request);
-        }
-
-        // Redirect jika pengguna bukan admin
-        return redirect('/')->with('error', 'You do not have access to this page.');
+        return $next($request);
     }
 }
